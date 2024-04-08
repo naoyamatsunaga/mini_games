e8f672c48b629f20e0a89dbe7f0ad6c3dec64c57
NaoyaMatsunaga <>
2024-04-08 23:36:57 +0900

    ヌメロン用ボタンを仮配置

--- lib/view/select_page.dart
+++ lib/view/select_page.dart
@@ -14,12 +14,9 @@ class SelectPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'Test',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              child: const Text('Numeron'),
+              onPressed: () {},
             ),
           ],
         ),
