1bb6bac44fb4915fde619c64c8352d258f3f95ff
NaoyaMatsunaga <>
2024-04-18 00:58:56 +0900

    答えと解答欄の位置を調整

--- lib/view/numeron_page.dart
+++ lib/view/numeron_page.dart
@@ -11,11 +11,15 @@ class NumeronPage extends StatelessWidget {
         title: const Text('Numeron'),
       ),
       body: Center(
+        //答え_START//////////////////////////////
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              child: const Row(
+            // 余白をつけたいWidgetはPaddingで囲むことができる
+            const Padding(
+              padding: EdgeInsets.all(20),
+              // padding: EdgeInsets.only(left: 0, top: 0, right: 0, bottom: 0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
@@ -24,17 +28,46 @@ class NumeronPage extends StatelessWidget {
                       fontSize: 50,
                     ),
                   ),
-                  Text('?'),
-                  Text('?'),
+                  Text(
+                    '?',
+                    style: TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                  Text(
+                    '?',
+                    style: TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
                 ],
               ),
             ),
-            Container(
-              width: 100,
-              child: TextField(
-                maxLength: 3,
-              ),
+            //答え_END//////////////////////////////
+
+            //リスト_START//////////////////////////////
+
+            //リスト_END//////////////////////////////
+
+            //回答欄_START//////////////////////////////
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(width: 80),
+                const SizedBox(
+                  width: 100,
+                  child: TextField(
+                    maxLength: 3,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("GO"),
+                )
+              ],
             ),
+            //回答欄_END//////////////////////////////
           ],
         ),
       ),
