1aa8fb244f121bd5400c15972a930b69933ab33c
NaoyaMatsunaga <>
2024-04-20 08:17:29 +0900

    回答リストを追加

--- lib/view/numeron_page.dart
+++ lib/view/numeron_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class NumeronPage extends StatelessWidget {
   const NumeronPage({super.key});
 
+  final answertextsize = 80.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +18,8 @@ class NumeronPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             // 余白をつけたいWidgetはPaddingで囲むことができる
-            const Padding(
-              padding: EdgeInsets.all(20),
+            Padding(
+              padding: const EdgeInsets.all(20),
               // padding: EdgeInsets.only(left: 0, top: 0, right: 0, bottom: 0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -25,19 +27,19 @@ class NumeronPage extends StatelessWidget {
                   Text(
                     '?',
                     style: TextStyle(
-                      fontSize: 50,
+                      fontSize: answertextsize,
                     ),
                   ),
                   Text(
                     '?',
                     style: TextStyle(
-                      fontSize: 50,
+                      fontSize: answertextsize,
                     ),
                   ),
                   Text(
                     '?',
                     style: TextStyle(
-                      fontSize: 50,
+                      fontSize: answertextsize,
                     ),
                   ),
                 ],
@@ -46,30 +48,68 @@ class NumeronPage extends StatelessWidget {
             //答え_END//////////////////////////////
 
             //リスト_START//////////////////////////////
-
+            Expanded(
+              flex: 1,
+              child: ListView(
+                children: [
+                  AnswerItem(),
+                ],
+              ),
+            ),
             //リスト_END//////////////////////////////
 
             //回答欄_START//////////////////////////////
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(width: 80),
-                const SizedBox(
-                  width: 100,
-                  child: TextField(
-                    maxLength: 3,
+            Padding(
+              padding: const EdgeInsets.all(50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(width: 100),
+                  const SizedBox(
+                    width: 100,
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 45,
+                      ),
+                      maxLength: 3,
+                    ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("GO"),
-                )
-              ],
+                  const SizedBox(width: 50),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("GO"),
+                  )
+                ],
+              ),
             ),
             //回答欄_END//////////////////////////////
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AnswerItem extends StatelessWidget {
+  const AnswerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      color: Colors.red,
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('No.'),
+          SizedBox(width: 50),
+          Text('XXX'),
+          SizedBox(width: 50),
+          Text('XEAT'),
+          SizedBox(width: 30),
+          Text('XBITE'),
+        ],
       ),
     );
   }
